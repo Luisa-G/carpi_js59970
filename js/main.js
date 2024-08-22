e8672d231167be2fab62bc58ae70d606d9a1584d
Luisa-G <luisa.gonzalezpico@gmail.com>
2024-08-21 20:55:57 -0600

    base del proyecto

---
+++ js/main.js
@@ -0,0 +1,21 @@
+/* Comentarios
+que pueden tener
+muchas líneas */
+
+// Comentarios de una línea
+
+//! Javascript es case sensitve
+//? pregunta azul azul
+//TODO naranja
+//* verde claro
+
+
+// let nombre = "Carpi";
+// const apellido = "Coder";
+
+let numero1 = 30;
+let numero2 = 2;
+
+let division = numero1/numero2;
+
+console.log(15);
