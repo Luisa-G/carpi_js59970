81ff7fb56f2ce901bb987711e76041a4ccc006c7
Luisa-G <luisa.gonzalezpico@gmail.com>
2024-09-25 14:03:52 -0600

    con sección de registro

--- js/main.js
+++ js/main.js
@@ -1,89 +1,89 @@
 // Registrar libros
 
-function registrarLibros() {
-    let libros = [];
-    let continuar = true;
+// function registrarLibros() {
+//     let libros = [];
+//     let continuar = true;
 
-    while (continuar === true) {
-        const titulo = prompt("¿Cuál es el nombre del libro?");
-        const autor = prompt("¿Cuál es el nombre del autor?");
-        let genero
-        do {
-            genero = prompt("¿A cuál género pertenece el libro?\n - Fantasía\n - Poesía\n - No ficción").toLowerCase();
-        } while (genero != "fantasía" && genero != "poesía" && genero != "no ficción");
-        let idioma 
-        do {
-            idioma = prompt("¿En qué idioma está escrito el libro?\n - Español\n - Inglés").toLowerCase();
-        } while (idioma != "español" && idioma != "inglés");
+//     while (continuar === true) {
+//         const titulo = prompt("¿Cuál es el nombre del libro?");
+//         const autor = prompt("¿Cuál es el nombre del autor?");
+//         let genero
+//         do {
+//             genero = prompt("¿A cuál género pertenece el libro?\n - Fantasía\n - Poesía\n - No ficción").toLowerCase();
+//         } while (genero != "fantasía" && genero != "poesía" && genero != "no ficción");
+//         let idioma 
+//         do {
+//             idioma = prompt("¿En qué idioma está escrito el libro?\n - Español\n - Inglés").toLowerCase();
+//         } while (idioma != "español" && idioma != "inglés");
 
-        libros.push({titulo: titulo, autor: autor, genero: genero, idioma: idioma});
+//         libros.push({titulo: titulo, autor: autor, genero: genero, idioma: idioma});
 
-        const otroLibro = prompt("¿Quieres agregar otro libro? Si/No").toLowerCase();
-        if (otroLibro === "si") {
-            continuar = true;    
-        } else {
-            continuar = false;
-        }
-    }
+//         const otroLibro = prompt("¿Quieres agregar otro libro? Si/No").toLowerCase();
+//         if (otroLibro === "si") {
+//             continuar = true;    
+//         } else {
+//             continuar = false;
+//         }
+//     }
 
-    return libros;
-}
-let librosRegistrados = registrarLibros();
-console.log(librosRegistrados);
-
-
-// Mostrar cuantos, y cuales, libros se registraron
-console.log(`Ingresaste un total de ${librosRegistrados.length} libros`);
-console.table(librosRegistrados);
+//     return libros;
+// }
+// let librosRegistrados = registrarLibros();
+// console.log(librosRegistrados);
 
 
-// Mostrar cuantos libros hay por género
-function titulosPorGenero(librosRegistrados){
-    let fantasia = [];
-    let poesia = [];
-    let noFiccion = [];
-
-    librosRegistrados.forEach((librosRegistrados) => {
-        if (librosRegistrados.genero === "fantasía") {
-            fantasia.push(librosRegistrados.titulo);
-        } else if (librosRegistrados.genero === "poesía") {
-            poesia.push(librosRegistrados.titulo);
-        } else {
-            noFiccion.push(librosRegistrados.titulo);
-        }
-    });
-
-    console.log("%c---LIBROS DE ACUERDO AL GÉNERO---", "color:orange");
-    console.log(`Tienes ${fantasia.length} libros de fantasía:`);
-    console.log(fantasia);
-    console.log(`Tienes ${poesia.length} libros de poesía:`);
-    console.log(poesia);
-    console.log(`Tienes ${noFiccion.length} libros de no ficción:`);
-    console.log(noFiccion);
-}
-
-titulosPorGenero(librosRegistrados);
+// // Mostrar cuantos, y cuales, libros se registraron
+// console.log(`Ingresaste un total de ${librosRegistrados.length} libros`);
+// console.table(librosRegistrados);
 
 
-// Mostrar cuantos libros por idioma
-function titulosPorIdioma(librosRegistrados){
-    let espaniol = [];
-    let ingles = [];
+// // Mostrar cuantos libros hay por género
+// function titulosPorGenero(librosRegistrados){
+//     let fantasia = [];
+//     let poesia = [];
+//     let noFiccion = [];
 
-    librosRegistrados.forEach((librosRegistrados) => {
-        if (librosRegistrados.idioma === "español") {
-            espaniol.push(librosRegistrados.titulo);
-        } else {
-            ingles.push(librosRegistrados.titulo);
-        }
-    });
+//     librosRegistrados.forEach((librosRegistrados) => {
+//         if (librosRegistrados.genero === "fantasía") {
+//             fantasia.push(librosRegistrados.titulo);
+//         } else if (librosRegistrados.genero === "poesía") {
+//             poesia.push(librosRegistrados.titulo);
+//         } else {
+//             noFiccion.push(librosRegistrados.titulo);
+//         }
+//     });
 
-    console.log("%c---LIBROS DE ACUERDO AL IDIOMA---", "color:blue");
-    console.log(`Tienes ${espaniol.length} libros en español:`);
-    console.log(espaniol);
-    console.log(`Tienes ${ingles.length} libros en inglés:`);
-    console.log(ingles);
-}
+//     console.log("%c---LIBROS DE ACUERDO AL GÉNERO---", "color:orange");
+//     console.log(`Tienes ${fantasia.length} libros de fantasía:`);
+//     console.log(fantasia);
+//     console.log(`Tienes ${poesia.length} libros de poesía:`);
+//     console.log(poesia);
+//     console.log(`Tienes ${noFiccion.length} libros de no ficción:`);
+//     console.log(noFiccion);
+// }
 
-titulosPorIdioma(librosRegistrados);
+// titulosPorGenero(librosRegistrados);
+
+
+// // Mostrar cuantos libros por idioma
+// function titulosPorIdioma(librosRegistrados){
+//     let espaniol = [];
+//     let ingles = [];
+
+//     librosRegistrados.forEach((librosRegistrados) => {
+//         if (librosRegistrados.idioma === "español") {
+//             espaniol.push(librosRegistrados.titulo);
+//         } else {
+//             ingles.push(librosRegistrados.titulo);
+//         }
+//     });
+
+//     console.log("%c---LIBROS DE ACUERDO AL IDIOMA---", "color:blue");
+//     console.log(`Tienes ${espaniol.length} libros en español:`);
+//     console.log(espaniol);
+//     console.log(`Tienes ${ingles.length} libros en inglés:`);
+//     console.log(ingles);
+// }
+
+// titulosPorIdioma(librosRegistrados);
 
